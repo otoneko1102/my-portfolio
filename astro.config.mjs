04ee93398af1c48c0b61c74be6f558a960a8df5e
otoneko1102 <pineapple2006.11.2@gmail.com>
2025-12-12 12:38:32 +0100

    サイトマップを追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,22 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import svelte from '@astrojs/svelte';
+import svelte from "@astrojs/svelte";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
   outDir: "./build",
   site: "https://montblank.fun",
-  integrations: [svelte()]
+  integrations: [
+    svelte(),
+    sitemap({
+      serialize(item) {
+        return item;
+      },
+      changefreq: "weekly",
+      priority: 1,
+    }),
+  ],
 });
